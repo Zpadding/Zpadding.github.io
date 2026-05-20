6d5946214b5c077c0569455346165fdb04f9600d
周腾飞 <17634626997@163.com>
2026-05-20 14:44:03 +0800

    fix: 打包目录修改

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: 'docs',
   },
 })
